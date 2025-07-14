7eb5673400a9d63972a8ae0ed5c14bf4dd6bc7b1
joegravelyn <joe@gravelyn.com>
2025-07-14 00:22:45 -0700

    Removing older race list logic. Copied to new _logic file

--- collect_inator/race_list_logic.py
+++ collect_inator/race_list_logic.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
-def json_to_df(json: object) -> pd.DataFrame:
-   df = pd.DataFrame(json) # type: ignore
+def json_to_df(json: dict | list | object) -> pd.DataFrame:
+   races = []
+   for series in json: # type: ignore
+      races += json[series] # type: ignore
+   df = pd.DataFrame(races)
    return df
